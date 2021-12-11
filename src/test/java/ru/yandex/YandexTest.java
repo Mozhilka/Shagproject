5139d3cdd2ddc30cad60e0f541b4860142f1cd27
o.saksonova@andersenlab.com <87995228+Mozhilka@users.noreply.github.com>
2021-12-11 20:34:49 +0300

    Auth

--- src/test/java/ru/yandex/YandexTest.java
+++ src/test/java/ru/yandex/YandexTest.java
@@ -10,29 +10,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
-import static io.netty.handler.codec.socksx.v5.Socks5AuthMethod.PASSWORD;
 
 public class YandexTest {
     WebDriver driver;
-
-    @BeforeEach
-    public void driverSetup() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
 
 
     @Test
     public void authTest1() {
 
-        driver.get("https://yandex.ru/");
-        WebElement mailButton = driver.findElement(By.xpath("//*[@class='desk-notif-card__login-new-item-title' and text()='Почта']"));
-        mailButton.click();
+//        driver.get("https://yandex.by");
+//        WebElement mailButton = driver.findElement(By.xpath("//*[@class='desk-notif-card__login-new-item-title' and text()='Почта']"));
+//        mailButton.click();
 
+        driver.get("https://passport.yandex.by");
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='text']")));
         WebElement emailInputField = driver.findElement(By.xpath("//input[@type='text']"));
         emailInputField.sendKeys("mozhilka@tut.by");
@@ -41,11 +35,39 @@ public class YandexTest {
         WebElement singInButton = driver.findElement(By.xpath("//button[@id='passp:sign-in']"));
         singInButton.click();
 
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='passp-field-passwd']")));
+        WebElement passwordInputField = driver.findElement(By.xpath("//input[@id='passp-field-passwd']"));
+        passwordInputField.sendKeys("jkmrf5151216");
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='passp:sign-in']")));
+        WebElement enterPasswordButton = driver.findElement(By.xpath("//button[@id='passp:sign-in']"));
+        enterPasswordButton.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@class='user-pic__image']")));
+        WebElement mailPage = driver.findElement(By.xpath("//img[@class='user-pic__image']"));
+        mailPage.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='menu__text']")));
+        WebElement menuText = driver.findElement(By.xpath("//span[@class='menu__text']"));
+        menuText.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='mail-Layout-Inner']")));
+
+        WebElement departmentResult = driver.findElement(By.xpath("//*[@class='mail-Layout-Inner']"));
+        Assert.assertTrue(departmentResult.getText().contains("Входящие"));
+
 
     }
     @AfterEach
     public void completed () {
         driver.quit();
+    }
+
+    @BeforeEach
+    public void driverSetup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
 }
